f8231e4a639c5c00144988977e9541980b4c7d70
4lex0akes <alexoakes@hotmail.co.uk>
2024-04-10 23:25:16 +0100

    Update Regular Expressions Password Genereator.py

--- Regular Expressions Password Genereator.py
+++ Regular Expressions Password Genereator.py
@@ -3,18 +3,27 @@
 # You can access the utilities defined inside the imported module through the dot notation. The dot notation works by appending a dot followed by the utility name to the module name. For example, here's how to access the "ascii_lowercase" constant:
 # It is a common convention to place import statements at the top of your code. And additionally, in case of multiple import statements, sort them in alphabetical order to improve readability.
 
+# The algorithm on which "random" relies makes the generated pseudo-random numbers predictable. Therefore, although the random module is suitable for the most common applications, it cannot be used for cryptographic purposes, due to its deterministic nature.
 import random
+
+import secrets
 import string
 
+def generate_password(length):
 # Define the possible characters for the password.
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
 
 # Next I will declare a variable named "all_characters" and assign it the result of concatenating my existing variables.
-all_characters = letters + digits + symbols
-print(all_characters)
-# "Call the "random()" function from the "random" module and print the result."
-print(random.random())
-# The "choice()"" function from the random module takes a sequence and returns a random item of the sequence.
-print(random.choice(all_characters))
+    all_characters = letters + digits + symbols
+    # For future reference, "''" represents an empty string. (Being assigned to the "password" variable in this example)
+    password = ''
+
+##########################
+# - print(all_characters)
+    # "Call the "random()" function from the "random" module and print the result."
+# - print(random.random())
+    # The "choice()"" function from the random module takes a sequence and returns a random item of the sequence.
+# - print(secrets.choice(all_characters))
+##########################
