be341e5d08006b1d2a7e26b39413f9c1179c9ded
4lex0akes <alexoakes@hotmail.co.uk>
2024-05-06 08:13:16 +0100

    Update Shortest Path Algorithm.py

--- Shortest Path Algorithm.py
+++ Shortest Path Algorithm.py
@@ -2,4 +2,10 @@ copper = {
     'species': 'guinea pig',
     'age': 2
 }
-print(copper['age'])
+
+# To add a new key-value pair after declaring a dictionary (above), you can indicate the key in the same way you would access an existing key, and set the value of the new key by using the assignment operator:
+copper['food'] = 'hay'
+# The same syntax can be used to change the value of an existing key within the dictionary as demonstrated below.
+copper['species'] = 'Cavia porcellus'
+
+print(copper)
